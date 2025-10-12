9d9c2a13aae2b1f5dad33799d68a259349a36587
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-10-13 00:31:57 +0300

    listing the date, Author name and title and implementing some links

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,32 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home(
-  {searchParams}:
-  {searchParams: 
-    {query?: string}
-  }) {
-    const query = (await searchParams).query
+  { searchParams }:
+    {
+      searchParams:
+      { query?: string }
+    }) {
+  const query = (await searchParams).query
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      _id: '1',
+      author: {
+        id: 1,
+        name: 'Taher'
+      },
+      description: 'this is the description',
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRG7HsYUU0xC3LVTs5w3csqTPx_4sr-ywREKg&s",
+      category: 'AI',
+      title: 'AI for all'
+
+
+    }
+  ]
   return (
     <>
 
@@ -16,14 +36,30 @@ export default async function Home(
         >
           My Children Achievments
           <br />
-          Taher & Mariam 
+          Taher & Mariam
         </h1>
         <p
           className="sub-heading !max-w-3xl"
         >
           Submit an Idea, vote on Achievment, Get noticed in the compitition
         </p>
-        <SearchForm query = {query}/>
+        <SearchForm query={query} />
+      </section>
+      <section>
+        <p>
+          {query ? `search for the ${query}` : "All Startups"
+          }
+        </p>
+        <ul className="mt-7 card-grid ">
+          {posts.length > 0
+            // TODO --> Make the types with Sanity lib
+            ? posts.map((post: any, index: number) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+            : <p>No posts found</p>}
+
+
+        </ul>
       </section>
     </>
   );
