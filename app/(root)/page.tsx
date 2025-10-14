b371ae9221aa919767281659077aecc12a63a7c3
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-10-14 13:31:57 +0300

    startup container code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -45,7 +45,7 @@ export default async function Home(
         </p>
         <SearchForm query={query} />
       </section>
-      <section>
+      <section className="section_container">
         <p>
           {query ? `search for the ${query}` : "All Startups"
           }
@@ -64,3 +64,4 @@ export default async function Home(
     </>
   );
 }
+
