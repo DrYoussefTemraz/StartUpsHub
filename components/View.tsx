4bc1718e0db1ee53342529587662d2588beeae46
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-10-22 11:52:35 +0300

    show the ISR incremental static regeneration and cash also use the PPR in views

--- components/View.tsx
+++ components/View.tsx
@@ -2,25 +2,36 @@ import React from 'react'
 import Ping from './ping'
 import { client } from '@/sanity/lib/client'
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+import { writeClient } from '@/sanity/lib/write-client'
+import { after } from 'next/server'
 
 
 async function View({ id }: { id: string }) {
-    const {views:totalViews} = await client
-    .withConfig({
-        useCdn: false
-    })
-    .fetch(STARTUP_VIEWS_QUERY, { id })
-    // update views
-    
+    const { views: totalViews } =
+        await client
+            .withConfig({
+                useCdn: false
+            })
+            .fetch(STARTUP_VIEWS_QUERY, { id })
+
+    // Update the views
+    after(async () => await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+    )
+
+    // Schedule the work after our response , update the views in background without blocking the UI
+
     return (
-    <div className='view-container'>
-        <div className='absolute -top-2 -right-2'>
-            <Ping />
+        <div className='view-container'>
+            <div className='absolute -top-2 -right-2'>
+                <Ping />
+            </div>
+            <p className='view-text'>
+                <span className='font-black'>views = {totalViews} </span>
+            </p>
         </div>
-        <p className='view-text'>
-            <span className='font-black'>views = {totalViews} </span>
-        </p>
-    </div>
     )
 
 }
