0cebeb15859b747f5c9f45410ec770eb2a8aa599
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-10-25 22:32:23 +0300

    hot fixes to the auth

--- auth.ts
+++ auth.ts
@@ -1,54 +1,58 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import { client } from "./sanity/lib/client"
-import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries";
-import { writeClient } from "./sanity/lib/write-client";
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+import { client } from "./sanity/lib/client";
+import { writeClient } from "@/sanity/lib/write-client";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
   callbacks: {
-    // check the user exist in database or not 
-    async signIn(
-      { user: { name, email, image },
-        profile: { id, login, bio }
-      }) {
+    async signIn({ user, profile }) {
+      // Check if the user exists in Sanity
       const existingUser = await client.withConfig({
-        // set the cdn to false to avoid the cashing and undefined id 
         useCdn: false,
-      }).fetch(AUTHOR_BY_GITHUB_ID_QUERY, { id });
-      if (!existingUser) {
-        // create user in database
+      }).fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+        id: profile?.id,
+      });
 
-        await writeClient.create(
-          {
-            _type: "author",
-            id,
-            name,
-            username: login,
-            email,
-            image,
-            bio,
-          }
-        )
+
+      if (!existingUser) {
+        // User not found in Sanity, create a new user document
+        await writeClient.create({
+          _type: "author",
+          id: profile?.id,
+          name: user?.name,
+          username: profile?.login,
+          email: user?.email,
+          image: user?.image,
+          bio: profile?.bio || "",
+        });
       }
+
+      // Return true to continue the sign-in process
       return true;
     },
-    // create the jwt token
     async jwt({ token, account, profile }) {
+      // Add profile id to the JWT token on sign-in
       if (account && profile) {
-        const user = await client.withConfig(
-          {
-            useCdn: false,
-          }
-        ).fetch(AUTHOR_BY_GITHUB_ID_QUERY, { id: profile.id });
-        token.id = user?._id;
+        // get the user from sanity
+        const user = await client.withConfig({
+          useCdn: false,
+        }).fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+          id: profile?.id,
+        });
+
+        // Extend the token with the GitHub profile id
+        token.id = user._id;
       }
       return token;
     },
-    // set the user in the session - expost the token new id into the sessions used in the pages
+
     async session({ session, token }) {
+      // Pass the profile id from the token to the session
       Object.assign(session, { id: token.id });
       return session;
     },
-  }
-})
+  },
+});
--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,8 +1,11 @@
+import NextAuth from "next-auth";
+
 declare module "next-auth" {
   interface Session {
-    id: string;
+    id: string; // Add the custom field here
   }
-  interface jwt {
-    id: string;
+
+  interface JWT {
+    id: string; // Add the custom field to the token
   }
 }
