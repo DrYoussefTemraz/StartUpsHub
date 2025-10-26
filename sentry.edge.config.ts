1a8ff88b8801ed99bbd5a924b25b9586fcbfaa54
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-10-26 17:03:15 +0300

    stop sentry tracking andreply

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -9,12 +9,12 @@ Sentry.init({
   dsn: "https://55ea126c6075b2f59ca73ab8f9aba4ed@o4510251807670272.ingest.us.sentry.io/4510251816779776",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: 0,
 
   // Enable logs to be sent to Sentry
-  enableLogs: true,
+  enableLogs: false,
 
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
+  sendDefaultPii: false,
 });
